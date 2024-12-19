b04a5606101beb67f604b164a7be274601a991e7
SwapPathak <117801866+swapnil15021997@users.noreply.github.com>
2024-12-19 17:40:55 +0530

    minor change
    minor change

--- resources/views/index_new.blade.php
+++ resources/views/index_new.blade.php
@@ -166,7 +166,7 @@
                 <div class="col-md-6 col-lg-4">
                 <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Latest Orders</h3>
+                            <h3 class="card-title">Latest Branch</h3>
                         </div>
                         <div class="card-table table-responsive">
                             <table class="table table-vcenter">
