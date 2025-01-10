12e1e43e5bc8bff5ef50d0987c110c3648abd29f
kasundra jeet <132254157+inlancersystem4@users.noreply.github.com>
2025-01-10 16:32:36 +0530

    tracker design

--- resources/views/orders/order_track.blade.php
+++ resources/views/orders/order_track.blade.php
@@ -19,73 +19,67 @@
                     </ol>
                 </nav>
             </div>
-            <div class="col-auto ms-auto d-print-none">
-                <div class="btn-list">
-                    @if(in_array(3, $user_permissions))
+        </div>
+    </div>
 
-                        <a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal"
-                            data-bs-target="#modal-report">
-                           
-                            Create new branch
-                        </a>
-                    @endif
-                    <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal"
-                        data-bs-target="#modal-report" aria-label="Create new report">
-                        <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M12 5l0 14" />
-                            <path d="M5 12l14 0" />
-                        </svg>
-                    </a>
+    <div class="card  my-4 mx-auto">
+        <div class="card-body">
+            <div class="timeline">
+                <div class="timeline-item completed">
+                    <h3>Ordered</h3>
+                    <p>15:30, September 9, 2018</p>
                 </div>
+
+                <div class="timeline-item completed">
+                    <h3>Shipped</h3>
+                    <p>15:45, September 9, 2018</p>
+                </div>
+
+                <div class="timeline-item">
+                    <h3>Delivered</h3>
+                    <p>Estimated delivery by 17:30</p>
+                </div>
+            </div>
+            <div class="tracking-info">
+                <p>Courier: <span>On Fleet</span></p>
+                <p>Tracking Number: <span>1234567890123456</span></p>
             </div>
         </div>
     </div>
-    <div class="page-body">
-         
-    <div class="container-xl">
-        <div class="order-from-page">
-            <div class="w-full">
-                <div>
-                    <div class="page-body">
-                        <div class="d-flex flex-column gap-3">
-                            <ul>
-                                <li>
-                                @php
 
-                                    $formattedDate = \Carbon\Carbon::parse($check_order['created_at'])->format('g:i, M j, Y');
-                                @endphp
 
-                                    Order Date -- {{$formattedDate}} <br/>
-                                    Created -- {{$check_order['order_user']['name']}} -- {{$check_order['from_branch']['branch_name']}} <br>
-                                    
-                                </li>
-                                @foreach($check_order['transactions'] as $transaction)
-                                @php
+    <div class="d-flex flex-column gap-3">
+        <ul>
+            <li>
+                @php
 
-                                    $formattedDate = \Carbon\Carbon::parse($transaction['trans_time'])->format('g:i, M j, Y');
-                                @endphp
+                    $formattedDate = \Carbon\Carbon::parse($check_order['created_at'])->format('g:i, M j, Y');
+                @endphp
 
-                                    <li>
-                                        Order Date -- {{$formattedDate }} <br/>
-                                        Transfered -- {{$transaction['trans_approved_by']['name']}} -- {{$transaction['trans_from']['branch_name']}} <br>
-                                        Accepted   -- {{$transaction['trans_user']['name']}} -- {{$transaction['trans_to']['branch_name']}}
-                                    
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>  
-                    </div>
-                </div>
-            </div>
-        </div>
+                Order Date -- {{$formattedDate}} <br />
+                Created -- {{$check_order['order_user']['name']}} --
+                {{$check_order['from_branch']['branch_name']}} <br>
+
+            </li>
+            @foreach($check_order['transactions'] as $transaction)
+                        @php
+
+                            $formattedDate = \Carbon\Carbon::parse($transaction['trans_time'])->format('g:i, M j, Y');
+                        @endphp
+
+                        <li>
+                            Order Date -- {{$formattedDate }} <br />
+                            Transfered -- {{$transaction['trans_approved_by']['name']}} --
+                            {{$transaction['trans_from']['branch_name']}} <br>
+                            Accepted -- {{$transaction['trans_user']['name']}} --
+                            {{$transaction['trans_to']['branch_name']}}
+
+                        </li>
+            @endforeach
+        </ul>
     </div>
-</div>
 
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
-@endsection
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+    @endsection
