e09a78bf1f6e3d643bc5916568a1ce1743011bba
kasundra jeet <132254157+inlancersystem4@users.noreply.github.com>
2025-01-10 15:14:31 +0530

    style changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,20 +17,6 @@
 
 
 
-        <div class="note-open-btn">
-            <button class="btn btn-tabler btn-ghost-secondary btn-icon" onclick="toogleNoteSheet()">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    class="icon icon-tabler icons-tabler-outline clipboard">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
-                    <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
-                    <path d="M9 12h6" />
-                    <path d="M9 16h6" />
-                </svg>
-            </button>
-        </div>
-
 
         <div id="my-qr-reader"
             style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 1000; background: rgba(0, 0, 0, 0.8);">
@@ -76,8 +62,7 @@
     <!-- <script src="https://unpkg.com/html5-qrcode"></script> -->
 
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/qr-scanner/1.4.2/qr-scanner.umd.min.js"></script>
-    <script src=
-        "https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js">
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js">
     </script>
     <style>
         @media (max-width:768px) {
--- resources/views/orders/order_edit.blade.php
+++ resources/views/orders/order_edit.blade.php
@@ -470,6 +470,20 @@
     </div>
 </div>
 
+<div class="note-open-btn">
+    <button class="btn btn-tabler btn-ghost-secondary btn-icon" onclick="toogleNoteSheet()">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            class="icon icon-tabler icons-tabler-outline clipboard">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+            <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
+            <path d="M9 12h6" />
+            <path d="M9 16h6" />
+        </svg>
+    </button>
+</div>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 
