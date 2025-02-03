7dfb1cef8f662e076841195d65a7a49d39d77001
SwapPathak <117801866+swapnil15021997@users.noreply.github.com>
2025-02-03 09:57:55 +0530

    name change
    name change

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Sonic')
-<img src="{{ asset('static/sonic-large.svg')}}" width="110" height="35" alt="Tabler"
+<img src="{{ asset('static/sonic-large.svg')}}" width="110" height="35" alt="Sonic"
 class="">
 @else
 {{ $slot }}
