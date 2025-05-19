e725bf1b28dceda62728422975cfa73a4d4ddf8d
Inlancer <swapnilpathak1997@gmail.com>
2025-05-19 12:06:09 +0530

    login change
    login change

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use App\Models\User;
 use DB;
 
 class AuthenticatedSessionController extends Controller
@@ -26,16 +27,27 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
         
-        $request->authenticate();
+
+        
+        $data = $request->all();
+        $get_data = User::get_data_by_email($data['email']);
+         
+        if (!$get_data){
+            // dd("ehk");
+            \Log::info("User not found: redirecting to login");
+      
+            return redirect()->route('login')->withErrors(['email' => 'Please register first'])->withInput();
+        }
         \Log::info(['Before Session Regeneration'=> session()->getId()]);
 
-
+        $request->authenticate();
         $request->session()->regenerate();
         \Log::info(['Session Regenerated' => session()->getId()]);
 
         // $sessionId = session()->getId();
 
         // return redirect()->intended(route('dashboard', absolute: false));
+        \Log::info("dawda");
         return redirect()->route('dashboard');
     }
 
